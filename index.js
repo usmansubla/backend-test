73aab063e80e178d8eacea95ab74eaf426199301
usmansubla <eausman745@gmail.com>
2025-11-07 19:08:41 +0530

    g

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/instagram',(req,res)=>{
+res.send("hi this is your instagram")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
